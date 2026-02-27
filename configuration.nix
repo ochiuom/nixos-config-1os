bdb41e2181f726b64964ed3ba37a97a0fc2a3921
ochiuom <achintyabera@gmail.com>
2026-02-27 11:32:18 +0530

    fprintd trying

--- configuration.nix
+++ configuration.nix
@@ -290,5 +290,11 @@
   };
   programs.fuse.userAllowOther = true;
 
+  services.fprintd = {
+  enable = true;
+  tod.enable = true;
+  tod.driver = pkgs.libfprint-2-tod1-vfs0090;
+  };
+
   system.stateVersion = "26.05";
 }
