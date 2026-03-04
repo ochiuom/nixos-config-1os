9bc031b13c17f79cd9ca175d7e348525877efaee
ochiuom <achintyabera@gmail.com>
2026-03-04 13:21:09 +0530

    fix: move nix-community cache to nix.settings, clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,6 @@
 {
   description = "ochinix-pc NixOS configuration";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
@@ -25,7 +16,6 @@
   outputs = inputs@{ self, nixpkgs, home-manager, disko, ... }:
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
   in {
     nixosConfigurations.ochinix-pc = nixpkgs.lib.nixosSystem {
       inherit system;
